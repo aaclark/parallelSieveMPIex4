33e876d8baf3bd52d6d974604a373a77d5901925
Alain Clark <aac.clark@gmail.com>
2024-10-28 02:03:11 +0100

    perftest scripts

---
+++ perftest_bcast_malloc.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+# Output file to store results
+output_file="benchmark_results_bcast_malloc.txt"
+
+# Number of threads to test
+proc_counts=(1 2 4 8 16 32 96)
+
+# Max
+max_n=(10000000 20000000 40000000 80000000 160000000 320000000)
+
+# Clear the output file before starting the benchmarks
+echo "" > $output_file
+
+typeset -i N procs
+# Run the benchmarks
+for procs in "${proc_counts[@]}"; do
+  for N in "${max_n[@]}"; do
+    # Set the number of threads for OpenMP
+    export OMP_NUM_THREADS=procs
+    echo "P=${procs}, N=${N}"
+
+    # Run the matrix_multiply program and capture the output
+    result=$(mpiexec -np "$procs" ./sieve_bcast_malloc "$N")
+
+    # Append the result to the output file
+    echo "$result" >> $output_file
+  done
+done
+
+echo "Benchmarking complete! Results written to $output_file"
